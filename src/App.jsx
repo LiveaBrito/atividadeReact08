7bdd3035662aa0c1d31dc9169a32f2a5b3bc70a2
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-20 01:38:21 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Usuario from './components/Usuario'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Usuario online={true}/>
+      <Usuario online={false}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Usuario.jsx
@@ -0,0 +1,7 @@
+function Usuario(props){
+
+    return <h1>{props.online? "Usuário Online!" : "Usuário Offline!"}</h1>
+
+}
+
+export default Usuario
